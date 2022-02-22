6ee275c02ed2fbba54d9c9d5edf198e3061148ff
Aleksandr <asp1rantzenevich@gmail.com>
2022-02-23 00:15:41 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import aiohttp
 
-DB_URL = 'postgresql://postgres:123@localhost:5432/parser' 
-BOT_TOKEN = '1631262089:AAEPH9EE4Kh8yBUgRFOVlbcMU5z99_tv4b4' 
-OLX_TOKEN = 'Bearer 4e0b662ad622f7c550fcee5c9c34b5c80ac8553d' 
-ADMIN_USERNAME = '@buratinosoft' 
+DB_URL = '' 
+BOT_TOKEN = '' 
+OLX_TOKEN = '' 
+ADMIN_USERNAME = '' 
 ADMINS = (,)  # если ид один, то запятая после ид ОБЯЗАТЕЛЬНА
 session = aiohttp.ClientSession()
